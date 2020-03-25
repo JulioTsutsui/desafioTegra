2a4a79d73b7b87943410e665567898202cfef00d
Julio Tsutsui <julio.tsutsui@gmail.com>
2020-03-25 13:24:07 -0300

    Falta de variável de configuração do banco

--- model/Livro.js
+++ model/Livro.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const configs = require("../configs.json");
 // models
 const LivroSchema = new mongoose.Schema({
 	title: String,
@@ -7,4 +8,4 @@ const LivroSchema = new mongoose.Schema({
 	stock: Number
 });
 
-module.exports = mongoose.model("Livro",LivroSchema,"tegrabooks");
+module.exports = mongoose.model("Livro",LivroSchema,configs.dbname);
